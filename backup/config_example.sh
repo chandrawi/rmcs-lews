2610ee2887fc9dbfa5803057997a4fc088dfcd67
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-04-21 06:24:05 +0700

    Add delete after backup feature

--- backup/config_example.sh
+++ backup/config_example.sh
@@ -1,6 +1,8 @@
 DB_URL=postgres://postgres:db_password@localhost:5432/rmcs_resource
 BACKUP_PERIOD=3600
 BACKUP_TABLE=data
-BACKUP_PATH=/home/username/backup
+BACKUP_DIRECTORY=/home/username/backup
 GROUP_DEVICE=
 GROUP_MODEL=
+REPLACE_STATUS=
+DELETE_FLAG=0
